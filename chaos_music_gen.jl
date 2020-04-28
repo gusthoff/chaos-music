705b6cccf74d36d06176a953dddef3b567c1f793
gusthoff <gusthoff@gusthoff.com>
2020-04-28 23:16:35 +0200

    Commenting out package installation

--- chaos_music_gen.jl
+++ chaos_music_gen.jl
@@ -23,8 +23,8 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-using Pkg; Pkg.add("DynamicalSystems")
-using Pkg; Pkg.add("MusicXML")
+# using Pkg; Pkg.add("DynamicalSystems")
+# using Pkg; Pkg.add("MusicXML")
 # using Pkg; Pkg.add("Plots")
 
 using DynamicalSystems
